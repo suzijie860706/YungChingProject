73dee246ce2bbcefadaed23bfb8421a36dfb8a12
Jack <suzijie860706@gmail.com>
2024-10-11 00:24:23 +0800

    add comment

--- YungChingProject/Services/ICustomerService.cs
+++ YungChingProject/Services/ICustomerService.cs
@@ -4,10 +4,35 @@ namespace YungChingProject.Services
 {
     public interface ICustomerService
     {
+        /// <summary>
+        /// 取得所有客戶資料
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        /// <summary>
+        /// 取得所有客戶資料
+        /// </summary>
+        /// <param name="customerId">客戶名稱</param>
+        /// <returns></returns>
         Task<Customer?> GetCustomerByIdAsync(string customerId);
+        /// <summary>
+        /// 新增客戶資料
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
         Task<Customer> CreateCustomerAsync(Customer customer);
+        /// <summary>
+        /// 更新客戶資料
+        /// </summary>
+        /// <param name="customerId">客戶名稱</param>
+        /// <param name="customer"></param>
+        /// <returns></returns>
         Task<Customer?> UpdateCustomerAsync(string customerId, Customer customer);
+        /// <summary>
+        /// 刪除客戶資料
+        /// </summary>
+        /// <param name="customerId">客戶名稱</param>
+        /// <returns></returns>
         Task<bool> DeleteCustomerAsync(string customerId);
     }
 }
